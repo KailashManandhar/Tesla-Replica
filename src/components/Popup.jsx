1a1de8d669ff30b2428f709035f1cad64d7002cb
Kailash Manandhar <manandharkailash1@gmail.com>
2026-07-06 19:52:28 +0545

    complete UI for language functionality

---
+++ src/components/Popup.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default function Popup(props) {
+    const [selected, setSelected] = React.useState(true)
+
+    function changeChosen(bool) {
+        if (selected) {
+            console.log("english selected")
+        }
+        else {
+            console.log("nepali selected")
+        }
+        if (bool != selected) {
+            setSelected(prev => !prev)
+        }
+
+    }
+
+    return (
+        <div className="app-container">
+            <div className="popup-overlay">
+
+                <div className="popup-box" onClick={(e) => e.stopPropagation()}>
+
+                    <div className="choose-language">
+                        <div className={`eng ${selected ? "chosen" : ""}`} onClick={() => changeChosen(true)}>
+                            <p>English</p></div>
+                        <div className={`nep ${!selected ? "chosen" : ""}`} onClick={() => changeChosen(false)}>
+                            <p>Nepali</p>
+                        </div>
+                    </div>
+                    <div className="lang-btn-grp">
+                        <button onClick={props.onClick} className="close-btn">
+                            Close
+                        </button>
+                        <button onClick={props.onClick} className="close-btn">
+                            Change
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+}
