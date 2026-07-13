bee74bd4779d95396c5e2debcd00bc9e52c6f4d6
Kailash Manandhar <manandharkailash1@gmail.com>
2026-07-13 10:38:39 +0545

    scroll to top works but no vid

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
-
+import ScrollTotop from "./ScrollToTop"
 const Layout = (props) => {
   return (
-    <>  
-        <Header trans = {props.trans}/>
-        <main>
-            <Outlet />
-        </main>
-        <Footer />
+    <>
+      <ScrollTotop />
+      <Header trans={props.trans} />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
 
     </>
   )
